9fba8749ff57eccf0e4aa7262d129f17b48b27dc
Antonyalistair@gmail.com <Antonyalistair@gmail.com>
2018-04-05 14:29:57 +0300

    js files

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//script to handle email subscription
+$(document).ready(function(){
+	$("#subscriber").submit(function(event){
+		var mail=$("#email").val();		
+		alert(mail+" has been successfully added to our emailing list. Thank you");
+		event.preventDefault();
+	});
+
+});
+
+//navbar fullscreen menu
+function openNav() {
+    document.getElementById("menunav").style.height = "100%";
+}
+
+function closeNav() {
+    document.getElementById("menunav").style.height = "0%";
+}
